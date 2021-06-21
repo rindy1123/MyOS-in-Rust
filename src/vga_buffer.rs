c6612575ff9e632c2943a6347bc89ebeb3e9bf82
rindy1123 <genryusinbaou@gmail.com>
2021-06-22 08:04:26 +0900

    feat: add writer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -29,3 +29,58 @@ impl ColorCode {
     ColorCode((background as u8) << 4 | (foreground as u8))
   }
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+  ascii_char: u8,
+  color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+  chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+  column_position: usize,
+  color_code: ColorCode,
+  buffer: &'static Buffer,
+}
+
+impl Writer {
+  pub fn write_byte(&mut self, byte: usize) {
+    match byte {
+      b'\n' => self.new_line(),
+      byte => {
+        if self.column_position >= BUFFER_WIDTH {
+          self.new_line();
+        }
+
+        let row = BUFFER_HEIGHT - 1;
+        let col = self.column_position;
+
+        let color_code = self.color_code;
+        self.buffer.chars[row][col] = ScreenChar {
+          ascii_char: byte,
+          color_code,
+        }
+        self.column_position += 1;
+      }
+    }
+  }
+
+  fn write_string (&mut self, string: &str) {
+    for byte in string.bytes() {
+      match byte {
+        0x20..=0x7e | b'\n' => self.write_byte(byte),
+        _ => self.write_byte(0xfe),
+      }
+    }
+  }
+
+  fn new_line(&mut self) {}
+}
