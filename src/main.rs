110ca55c08bec2b6d070f2fe803ddb4daffca173
rindy1123 <genryusinbaou@gmail.com>
2021-06-26 09:38:39 +0900

    feat: enable tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -13,9 +16,19 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, World, {}", 12345);
-    panic!("Something went wrong");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
 mod vga_buffer;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
