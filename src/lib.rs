34d1d5a30fc07697e2d82be4c1fc8e20e414edd4
rindy1123 <genryusinbaou@gmail.com>
2021-07-09 09:19:39 +0900

    feat: enable hardware interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,8 @@ where
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 pub fn test_runner(tests: &[&dyn Testable]) {
