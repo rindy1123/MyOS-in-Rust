537bc18ba76c1b9dc8fcc94850b498ecfd3efb20
rindy1123 <genryusinbaou@gmail.com>
2021-07-13 07:54:39 +0900

    feat: iterate over level 4 table entries

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,21 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use os_project::memory::active_level_4_table;
+    use x86_64::VirtAddr;
+
     println!("Hello, World");
 
     os_project::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    for (i, entry) in l4_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("L4 Entry {}: {:?}", i, entry);
+        }
+    }
 
     #[cfg(test)]
     test_main();
